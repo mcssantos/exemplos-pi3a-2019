44cfc32fb4e53998c47c69ad9eb03d12f4649cd8
mcssantos <>
2019-08-16 22:00:08 -0300

    Nova Funcionalidade

--- gerenciador-produtos/src/main/java/br/senac/tads/pi3/gerenciadorprodutos/TesteGit.java
+++ gerenciador-produtos/src/main/java/br/senac/tads/pi3/gerenciadorprodutos/TesteGit.java
@@ -13,6 +13,11 @@ public class TesteGit {
     
     public static void main(String[] args) {
         System.out.println(" TESTES GIT");
+        
+        for (int i = 0; i<100; i++){
+        
+            System.out.println("Numero: "+i);
+        }
     }
     
 }
